f32349c516ca39aaad734c3fc0ab77eb17e6cb42
xspired01 <don.spire1@gmail.com>
2017-01-23 09:53:15 -0800

    Second time is a charm

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// Your JS goes here
+//make checkerboard using only javascript
+
+for (var i =0; i < 63; i++){
+  if (i%2===0){
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+  div.style.width = '11.1%';
+  div.style.float = 'left';
+  div.style.paddingBottom = '11.1%';
+  div.style.backgroundColor = "red";
+  } else {
+  var div = document.createElement('div');
+  document.body.appendChild(div);
+  div.style.width = '11.1%';
+  div.style.float = 'left';
+  div.style.paddingBottom = '11.1%';
+  div.style.backgroundColor = "black";
+  }
+  // console.log(document.body);
+
+}
