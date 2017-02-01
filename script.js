9f9cc983b2cfffb41efa8982b29c1305480933c5
xspired01 <don.spire1@gmail.com>
2017-01-31 16:06:43 -0800

    Linted

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //make checkerboard using only javascript
 
-for (var i =0; i < 63; i++){
-  if (i%2===0){
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  div.style.width = '11.1%';
-  div.style.float = 'left';
-  div.style.paddingBottom = '11.1%';
-  div.style.backgroundColor = "red";
+for (var i = 0; i < 63; i++){
+  if ( i % 2 === 0 ){
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    div.style.backgroundColor = "red";
   } else {
-  var div = document.createElement('div');
-  document.body.appendChild(div);
-  div.style.width = '11.1%';
-  div.style.float = 'left';
-  div.style.paddingBottom = '11.1%';
-  div.style.backgroundColor = "black";
+    var div = document.createElement('div');
+    document.body.appendChild(div);
+    div.style.width = '11.1%';
+    div.style.float = 'left';
+    div.style.paddingBottom = '11.1%';
+    div.style.backgroundColor = "black";
   }
   // console.log(document.body);
 
